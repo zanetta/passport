b967a760dcba5a4ea2eb7e535b3756941e01aebc
Glitch (atletas) <none>
2019-09-27 21:55:33 +0000

    🐏🌌 Updated with Glitch

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+const dbPassword = 'mongodb+srv://lutadoresapp:'+ encodeURIComponent('p0rr4docaraleo') + '@lutadoresapp-0mvov.mongodb.net/test?retryWrites=true';
 
 module.exports = {
     mongoURI: dbPassword
